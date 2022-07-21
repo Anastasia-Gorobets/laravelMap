77002161899378ef079127e1b5bcaa74f3d01ff9
nasty <nastya.gorobets95@gmail.com>
2022-07-21 22:12:44 +0300

    Added map changes

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Cornford\Googlmapper\Mapper;
 use Illuminate\Http\Request;
 
 class GoogleController extends Controller
@@ -13,6 +14,13 @@ class GoogleController extends Controller
     {
 
         return view('map',['users'=>User::get()->pluck('address')]);
+    }
+
+    public function map2()
+    {
+        Mapper::map(53.381128999999990000, -1.470085000000040000);
+
+        return view('map2');
     }
 
 
